bfaf1459708ee356c5836047715468c0ddbca071
squilliam12 <wfan@wm.edu>
2026-04-18 22:58:30 -0400

    Kalman Filter

--- Round 2/round_2_trader.py
+++ Round 2/round_2_trader.py
@@ -1,5 +1,6 @@
 import json
 from typing import Any
+import numpy as np
 
 from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
 
@@ -135,6 +136,55 @@ class Logger:
 
 logger = Logger()
 
+class KalmanFilter(object):
+    def __init__(self, F = None, Z = None, Q = None, eps = None, P = None, x0 = None):
+
+        if(F is None or Z is None):
+            raise ValueError("Set proper system dynamics.")
+
+        self.n = F.shape[1]
+        self.m = Z.shape[1]
+
+        # The transition matrix, set F = 1 - like a random walk
+        self.F = F
+
+        # Observation model, set Z = 1
+        self.Z = Z
+
+        # Process noise - Hidden Pattern
+        self.Q = np.eye(self.n) if Q is None else Q
+
+        # Measurement noise
+        self.eps = np.eye(self.n) if eps is None else eps
+
+        # Error covariance
+        self.P = np.eye(self.n) if P is None else P
+
+        # state estimate
+        self.x = np.zeros((self.n, 1)) if x0 is None else x0
+
+    def predict(self, u = 0):
+        self.x = np.dot(self.F, self.x) 
+        self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+        return self.x
+
+    def update(self, z):
+        y = z - np.dot(self.Z, self.x)
+        S = self.eps + np.dot(self.Z, np.dot(self.P, self.Z.T))
+        K = np.dot(np.dot(self.P, self.Z.T), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        I = np.eye(self.n)
+        self.P = np.dot(np.dot(I - np.dot(K, self.Z), self.P), 
+        	(I - np.dot(K, self.Z)).T) + np.dot(np.dot(K, self.eps), K.T)
+
+    def get_eps(self):
+        return self.eps
+
+    def get_P(self):
+        return self.P
+
+    def get_Q(self):
+        return self.Q
 
 class Trader:
     def __init__(self):
